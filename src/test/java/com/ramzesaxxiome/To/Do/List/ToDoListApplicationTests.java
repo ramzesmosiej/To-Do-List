4eb151e7786fb3ad90275bbea45e0c2ef08d74dc
Ramzes Mosiej <ramzes.mosiej@axxiomehealth.com>
2022-08-03 09:33:57 +0200

    Initial commit

---
+++ src/test/java/com/ramzesaxxiome/To/Do/List/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramzesaxxiome.To.Do.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
